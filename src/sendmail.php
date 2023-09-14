997028fa3abe699ed4ca5a322ec656760bdd14dd
Lemdorot <lemdorot@gmail.com>
2023-09-14 17:50:35 +0300

    Тестовое задание

---
+++ src/sendmail.php
@@ -0,0 +1,83 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+	require 'phpmailer/src/SMTP.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	
+	$mail->isSMTP();                                            //Send using SMTP
+	$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'lemdotestmail@gmail.com';                     //SMTP username
+	$mail->Password   = 'wsbpxudyrnvzjhvi';                               //SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+	$mail->Port       = 465;                 
+	
+
+	//От кого письмо
+	$mail->setFrom('lemdotestmail@gmail.com', 'Тема'); // Указать нужный E-mail
+	//Кому отправить
+	$mail->addAddress('lemdorot@gmail.com'); // Указать нужный E-mail
+	//Тема письма
+	$mail->Subject = 'Тестовое письмо"';
+
+	//Рука
+	$hand = "Правая";
+	if($_POST['hand'] == "left"){
+		$hand = "Левая";
+	}
+
+	//Тело письма
+	$body = '<h1>Письмо отправленное с тестового задания</h1>';
+
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>Email:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['hand']))){
+		$body.='<p><strong>Рука</strong> '.$hand.'</p>';
+	}
+	if(trim(!empty($_POST['age']))){
+		$body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+	
+	
+	//Прикрепить файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//путь загрузки файла
+		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
+		//грузим файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото в приложении</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+	
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
